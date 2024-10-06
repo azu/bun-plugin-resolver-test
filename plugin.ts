6b33dd58c2a733cf507aff710e42ab94f05345fe
azu <azuciao@gmail.com>
2024-10-06 21:05:26 +0900

    Delete index.ts+2

---
+++ plugin.ts
@@ -0,0 +1,30 @@
+import { plugin, type PluginBuilder } from "bun";
+
+await plugin({
+  name: "bun-test",
+  target: "bun",
+  setup(build: PluginBuilder): void | Promise<void> {
+    build.onResolve({ filter: /.*/ }, async (args) => {
+      console.log("resolve", args);
+      return args
+    });
+    build.onLoad({ filter: /.*/ }, async (args) => {
+      console.log("onload", args);
+      return {
+        contents: "console.log('Hello, world!')",
+        loader: "ts",
+      };
+    });
+  }
+});
+
+// resolve
+console.group("resolve")
+import.meta.resolve("rambda");
+console.groupEnd()
+
+// import
+console.group("import")
+import "rambda";
+
+console.groupEnd()
